6356ad6b3b62eba0a4fcfb1bdfec27d0dd543867
Christian Stimming <christian@cstimming.de>
2016-03-01 22:15:20 +0100

    Code cleanup: Better variable naming. Using unsigned long type correctly.

--- door_arduino.ino
+++ door_arduino.ino
@@ -12,17 +12,17 @@
  */
 
 // Names for the output pins:
-const int outMotorOn = 7;
-const int outMotorUp = 6;
-const int outWarnLight = 5;
-const int outRoomLight = 4;
-const int outArduinoLedPin = 13;
+const int pinOutMotorOn = 7;
+const int pinOutMotorUp = 6;
+const int pinOutWarnLight = 5;
+const int pinOutRoomLight = 4;
+const int pinOutArduinoLedPin = 13;
 
-const int inButton = 3;
-const int inButtonDown = 2;
-//const int inButtonInside =
-const int inAnalogPinOutsideButton = 0;
-const int inAnalogPinPhotosensor = 5; // right now unused!
+const int pinInButton = 3;
+const int pinInButtonDown = 2;
+//const int pinInButtonInside =
+const int pinAnalogInOutsideButton = 0;
+const int pinAnalogInPhotosensor = 5; // right now unused!
 
 // The relays are active-low, so we better define readable names for this
 #define RELAY_ON LOW
@@ -40,21 +40,27 @@ enum State {
 };
 /** This is the door's actual state */
 State state = DOOR_DOWN;
-long lastMoveStart = 0;
-const long moveDurationTotal =
+unsigned long lastMoveStart = 0;
+const unsigned long moveDurationTotal =
 #ifdef DEBUG
   2000;
 #else
-  18000; // total movement takes this milliseconds; DEBUG: 2000
+  18000L; // total movement takes this milliseconds; DEBUG: 2000
 #endif
-const long moveTurnaroundPause = 300; // extra waiting time when switching from one direction to the other
-const long waitingTimeBeforeReclose =
+const unsigned long moveTurnaroundPause = 300; // extra waiting time when switching from one direction to the other
+const unsigned long waitingTimeBeforeReclose =
 #ifdef DEBUG
-    5*1000;
+    5*1000L;
 #else
     600*1000L; // don't forget the trailing "L"!!!
 #endif
 // 10 minutes before reclose; DEBUG: 5 seconds
+const unsigned long waitingTimeBeforeReallyReclose =
+#ifdef DEBUG
+  3*1000L;
+#else
+  10*1000L; // 10 seconds of warning; DEBUG: 3 seconds
+#endif
 
 // The wrappers for the input buttons debouncing
 Bounce inButtonDebounce;
@@ -77,29 +83,29 @@ inline void outRoomLightSwitchOn() {
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  pinMode(outMotorOn, OUTPUT);
-  pinMode(outMotorUp, OUTPUT);
-  outWarnLightTimer.setupPin(outWarnLight);
+  pinMode(pinOutMotorOn, OUTPUT);
+  pinMode(pinOutMotorUp, OUTPUT);
+  outWarnLightTimer.setupPin(pinOutWarnLight);
   outWarnLightTimer.fade(blinkTime, blinkTime, blinkTime, blinkTime, 2);
 
-  outRoomLightTimer.setupPin(outRoomLight, true, true); // digital=true, inverted=true
+  outRoomLightTimer.setupPin(pinOutRoomLight, true, true); // digital=true, inverted=true
   outRoomLightSwitchOn();
-  outArduinoLed.setupPin(outArduinoLedPin, true); // digital=true
+  outArduinoLed.setupPin(pinOutArduinoLedPin, true); // digital=true
   outArduinoLed.blink(2000, 2000); // In DOOR_DOWN, do some slow blinking to show we are alive
 
-  digitalWrite(outMotorOn, RELAY_OFF);
-  digitalWrite(outMotorUp, RELAY_OFF);
+  digitalWrite(pinOutMotorOn, RELAY_OFF);
+  digitalWrite(pinOutMotorUp, RELAY_OFF);
 
-  pinMode(inButton, INPUT_PULLUP);
-  inButtonDebounce.attach(inButton);
+  pinMode(pinInButton, INPUT_PULLUP);
+  inButtonDebounce.attach(pinInButton);
   inButtonDebounce.interval(debounceDelay);
 
-  pinMode(inButtonDown, INPUT_PULLUP);
-  inButtonDownDebounce.attach(inButtonDown);
+  pinMode(pinInButtonDown, INPUT_PULLUP);
+  inButtonDownDebounce.attach(pinInButtonDown);
   inButtonDownDebounce.interval(debounceDelay);
 
   // The input button that has a potentiometer behaviour (due to humidity)
-  inButtonOutsideDebounce.attach(inAnalogPinOutsideButton);
+  inButtonOutsideDebounce.attach(pinAnalogInOutsideButton);
   inButtonOutsideDebounce.interval(debounceDelay);
   inButtonOutsideDebounce.setCurrentAsMax(); // calibrate the current "high" value of the outside button
 
@@ -107,13 +113,7 @@ void setup() {
   Serial.begin(9600);
 #endif
   doorUpStartReclose.setTimeout(waitingTimeBeforeReclose);
-  doorUpReallyReclose.setTimeout(
-#ifdef DEBUG
-  3*1000
-#else
-  10*1000
-#endif
-  ); // 10 seconds of warning; DEBUG: 3 seconds
+  doorUpReallyReclose.setTimeout(waitingTimeBeforeReallyReclose);
   doorUpStartReclose.stop();
   doorUpReallyReclose.stop();
 }
@@ -142,10 +142,6 @@ void loop() {
   val = analogRead(inAnalogPinPhotosensor);
   Serial.println(val);
 
-  Serial.print("OutsidePoti = ");
-  val = analogRead(inAnalogPinOutsideButton);
-  Serial.println(val);
-
   if (onInButtonOutsideChanged) {
     Serial.print("onInButtonOutsideChanged  -- ");
   }
@@ -166,8 +162,8 @@ void loop() {
   switch (state) {
     case DOOR_DOWN:
       // Door down/closed: Make sure motor is off
-      digitalWrite(outMotorOn, RELAY_OFF);
-      digitalWrite(outMotorUp, RELAY_OFF);
+      digitalWrite(pinOutMotorOn, RELAY_OFF);
+      digitalWrite(pinOutMotorUp, RELAY_OFF);
       // State change: Only when the Up-Button is pressed
       if (onInButtonPressed) {
         state = DOOR_MOVING_UP;
@@ -179,8 +175,8 @@ void loop() {
 
     case DOOR_UP:
       // Door up/open: Make sure motor is off
-      digitalWrite(outMotorOn, RELAY_OFF);
-      digitalWrite(outMotorUp, RELAY_OFF);
+      digitalWrite(pinOutMotorOn, RELAY_OFF);
+      digitalWrite(pinOutMotorUp, RELAY_OFF);
       // Special rule on Up-Button, if the reclose blinking is ongoing: Cancel the reclose
       if (doorUpReallyReclose.isActive() && onInButtonPressed) {
         doorUpReallyReclose.stop();
@@ -206,8 +202,8 @@ void loop() {
 
     case DOOR_MOVING_DOWN:
       // Door moving downwards/closing
-      digitalWrite(outMotorOn, RELAY_ON);
-      digitalWrite(outMotorUp, RELAY_OFF);
+      digitalWrite(pinOutMotorOn, RELAY_ON);
+      digitalWrite(pinOutMotorUp, RELAY_OFF);
       // Check for timeout so that we are down/closed
       if (millis() - lastMoveStart > moveDurationTotal) {
         state = DOOR_DOWN;
@@ -217,7 +213,7 @@ void loop() {
       // State change: Pressed button for changing direction?
       if (onInButtonPressed) {
         state = DOOR_MOVING_UP;
-        digitalWrite(outMotorOn, RELAY_OFF);
+        digitalWrite(pinOutMotorOn, RELAY_OFF);
         delay(moveTurnaroundPause);
         const unsigned long moveRemaining = moveDurationTotal - (millis() - lastMoveStart);
         lastMoveStart = millis() - moveRemaining + moveTurnaroundPause;
@@ -226,8 +222,8 @@ void loop() {
 
     case DOOR_MOVING_UP:
       // Door moving upwards/opening
-      digitalWrite(outMotorOn, RELAY_ON);
-      digitalWrite(outMotorUp, RELAY_ON);
+      digitalWrite(pinOutMotorOn, RELAY_ON);
+      digitalWrite(pinOutMotorUp, RELAY_ON);
       // Check for timeout so that we are up/opened
       if (millis() - lastMoveStart > moveDurationTotal) {
         state = DOOR_UP;
@@ -239,7 +235,7 @@ void loop() {
       // State change: Pressed button for changing direction?
       if (onInButtonDownPressed) {
         state = DOOR_MOVING_DOWN;
-        digitalWrite(outMotorOn, RELAY_OFF);
+        digitalWrite(pinOutMotorOn, RELAY_OFF);
         delay(moveTurnaroundPause);
         const unsigned long moveRemaining = moveDurationTotal - (millis() - lastMoveStart);
         lastMoveStart = millis() - moveRemaining + moveTurnaroundPause;
